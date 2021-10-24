625b1709a7edb45ef7e703feb996bb482bfd08a4
Md Shah Jalal <bestwebdesignerr@gmail.com>
2021-10-24 17:26:36 +0600

    last comment

--- index.js
+++ index.js
@@ -44,6 +44,23 @@ async function run() {
             res.json(result);
         })
 
+        // UPDATE/ PUT API
+        app.put('/users/:id', async (req, res) => {
+            const id = req.params.id;
+            const updatedUser = req.body;
+            const filter = { _id: ObjectId(id) };
+            const options = { upsert: true };
+            const updateDoc = {
+                $set: {
+                    name: updatedUser.name,
+                    email: updatedUser.email
+                }
+            };
+            const result = await usersCollection.updateOne(filter, updateDoc, options)
+            console.log('updating user', id);
+            res.json(result);
+        })
+
         //  DELETE API
         app.delete('/users/:id', async (req, res) => {
             const id = req.params.id;
